36d7f9d98243f6aaa55f645e6e241d6a31a81b6d
hgjeong <cubic0915@naver.com>
2024-04-28 19:47:04 +0900

    Feat : Add test_to_json function and integrate with main file

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, request
 from flask_restx import Resource, Api
 from flask_cors import CORS
 
-from uitls.generator import generate
-
+from utils import model
+from utils import order_processing
 
 app = Flask(__name__)
 CORS(app)
@@ -12,9 +12,9 @@ api = Api(app)
 @api.route('/text/<string:text>')
 class TodoSimple(Resource):
     def get(self, text):
-        return {
-            'genetrated': generate(text),
-        }
+        generated_text = model.generate(text)
+        processed_text = order_processing.text_to_json(generated_text)
+        return processed_text
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
