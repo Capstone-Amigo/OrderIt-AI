dd2c1423ecccebd1a773c47f16b5e1c113177ba2
hgjeong <cubic0915@naver.com>
2024-05-12 15:22:33 +0900

    Refactor: Rename order_processsing.py to order_processs.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_restx import Resource, Api
 from flask_cors import CORS
 
 from utils import model
-from utils import order_processing
+from utils import order_process
+from utils import menu
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +14,7 @@ api = Api(app)
 class TodoSimple(Resource):
     def get(self, text):
         generated_text = model.generate(text)
-        processed_text = order_processing.text_to_json(generated_text)
+        processed_text = order_process.text_to_json(generated_text)
         return processed_text
 
 if __name__ == "__main__":
--- utils/order_process.py
+++ utils/order_process.py
@@ -1,5 +1,4 @@
-import re
-
+from utils import menu
 
 def text_to_json(text):
     if text is None:
@@ -15,19 +14,20 @@ def text_to_json(text):
         if command.strip():
             # 괄호를 기준으로 메뉴명과 파라미터 분리
             parts = command.strip().split('(')
-            menu = parts[0].strip()
+            menu_text = parts[0].strip()
             # 파라미터 부분에서 ','로 파라미터를 분리
             params = parts[1].strip(')').split(',')
             order_details = {}
-            order_details['menu'] = menu
+            order_details['menu'] = menu_text
 
             # 각 파라미터에서 '='로 키와 값을 분리
             for param in params:
                 key, value = param.split('=')
                 order_details[key.strip()] = value.strip()
 
+            if menu_text not in menu.get_menu_list():
+                continue
+            
             orders.append(order_details)
 
     return {'Order': orders}
-
-
